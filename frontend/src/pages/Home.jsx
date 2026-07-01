122df4439f6398cdf357a8f5f8c3dc2c5b2a3ecf
bradgerb <77023448+bradgerb@users.noreply.github.com>
2026-07-01 13:37:42 -0400

    Update home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import odinImg from "../img/odin.png";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function Dashboard() {
 
   const [posts, setPosts] = useState([]);
-
+  const { user, logout } = useAuth();
 
   useEffect(() => {
     let cancelled = false;
@@ -35,24 +37,38 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <div>
-      <h1>Home page</h1>
-      <p>Welcome to Odinbook!</p>
-      {posts.length === 0 ? (
-        <p>No posts yet</p>
-      ) : (
-        <div>
-          <ul>
-            {posts.map((post) => (
-              <li key={post.id}>
-                <Link to={`/posts/${post.id}`}>
-                  <div>Title: {post.title} - Author: {post.author?.username ?? "deleted user"} - nrofcomments: {post._count.comments}</div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+    <>
+      {!user ? (
+        <div className="center">
+          <h1>Home page</h1>
+          <p>Welcome to Odinbook!</p>
+          <img src={odinImg} alt="Odin image" className="odinImage" height={200}/>
         </div>
+      ) : (
+        <>
+          <div>
+            New post form
+          </div>
+          <br />
+          <div>
+            {posts.length === 0 ? (
+              <p>No posts yet</p>
+            ) : (
+              <div>
+                <ul>
+                  {posts.map((post) => (
+                    <li key={post.id}>
+                      <Link to={`/posts/${post.id}`}>
+                        <div>Title: {post.title} - Author: {post.author?.username ?? "deleted user"} - nrofcomments: {post._count.comments}</div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </>
       )}
-    </div>
+    </>
   );
 }
